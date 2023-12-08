693800e2cedf3853fff8639a2cba9f79b79c7e6c
Robin van der Noord <robinvandernoord@gmail.com>
2023-12-08 10:25:01 +0100

    day 8 pt2

--- 8/day8.go
+++ 8/day8.go
@@ -74,6 +74,150 @@ func solve(instructions string, mapping map[string]Tuple) int {
 	return rounds
 }
 
+// func threaded(threadno int, ch_recv chan rune, ch_send chan bool, location string, mapping map[string]Tuple) {
+// 	fmt.Printf("new thread! %d\n", threadno)
+
+// 	for instruction := range ch_recv {
+// 		if instruction == 'X' {
+// 			break
+// 		} else if instruction == 'L' {
+// 			location = mapping[location].L
+// 		} else if instruction == 'R' {
+// 			location = mapping[location].R
+// 		}
+
+// 		done := location[len(location)-1] == 'Z'
+// 		if done {
+// 			fmt.Printf("%d - %s\n", threadno, location)
+// 		}
+
+// 		ch_send <- done
+// 	}
+// 	fmt.Printf("%d byebye\n", threadno)
+// }
+
+// type Thread struct {
+// 	threadno int
+// 	ch_send  chan rune
+// 	ch_recv  chan bool
+// }
+
+// func solve2(instructions string, mapping map[string]Tuple) int {
+
+// 	starting_positions := make([]string, 0, 5)
+// 	for position := range mapping {
+// 		if position[len(position)-1] == 'A' {
+// 			starting_positions = append(starting_positions, position)
+// 		}
+// 	}
+
+// 	threads := make([]Thread, 0, 5)
+
+// 	for x := 0; x < len(starting_positions); x++ {
+// 		fmt.Printf("thread: %d\n", x)
+// 		ch_send := make(chan rune) // Creating an unbuffered channel
+// 		ch_recv := make(chan bool) // Creating an unbuffered channel
+
+// 		go threaded(x, ch_send, ch_recv, starting_positions[x], mapping)
+// 		threads = append(threads, Thread{x, ch_send, ch_recv})
+// 	}
+
+// 	rounds := 0
+
+// 	for true {
+// 		instruction := instructions[rounds%len(instructions)]
+// 		rounds += 1
+// 		all := true
+
+// 		for _, thread := range threads {
+// 			thread.ch_send <- rune(instruction)
+// 		}
+
+// 		for _, thread := range threads {
+// 			thread_result := <-thread.ch_recv
+// 			if thread_result == false {
+// 				all = false
+// 			}
+// 		}
+
+// 		if all {
+// 			break
+// 		}
+// 	}
+
+// 	// clean up:
+// 	for _, thread := range threads {
+// 		thread.ch_send <- 'X'
+// 	}
+
+// 	return rounds
+// }
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func findSmallestDivisible(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+	result := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		result = lcm(result, numbers[i])
+	}
+	return result
+}
+
+func solve2(instructions string, mapping map[string]Tuple) int {
+	starting_positions := make([]string, 0, 5)
+	for position := range mapping {
+		if position[len(position)-1] == 'A' {
+			starting_positions = append(starting_positions, position)
+		}
+	}
+
+	options := make([]int, 0, 5)
+	for _, start := range starting_positions {
+		rounds := 0
+
+		first_match := 0
+
+		for x := 0; x < 10_000_000_000; x++ {
+			instruction := instructions[rounds%len(instructions)]
+			rounds += 1
+
+			if instruction == 'L' {
+				start = mapping[start].L
+			} else if instruction == 'R' {
+				start = mapping[start].R
+			} else {
+				panic("Invalid instruction!")
+			}
+
+			if start[len(start)-1] == 'Z' {
+
+				if first_match == 0 {
+					first_match = rounds
+				} else if rounds%first_match == 0 {
+					// it looped!
+					break
+				}
+
+				options = append(options, rounds)
+			}
+		}
+	}
+
+	return findSmallestDivisible(options)
+}
+
 func run(filename string) int {
 	contents := readFile(filename)
 
@@ -82,4 +226,14 @@ func run(filename string) int {
 	mapping := getMapping(rows)
 
 	return solve(instructions, mapping)
+}
+
+func run2(filename string) int {
+	contents := readFile(filename)
+
+	instructions, rows := split(contents, "\n\n")
+
+	mapping := getMapping(rows)
+
+	return solve2(instructions, mapping)
 }
--- 8/day8_test.go
+++ 8/day8_test.go
@@ -1,45 +1,45 @@
 package day8
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestDev1(t *testing.T) {
-	result := run("dev1.txt")
+// func TestDev1(t *testing.T) {
+// 	result := run("dev1.txt")
 
-	if result != 2 {
-		t.Fatalf("Incorrect result! %d != 2", result)
-	}
+// 	if result != 2 {
+// 		t.Fatalf("Incorrect result! %d != 2", result)
+// 	}
 
-}
+// }
 
-func TestDev2(t *testing.T) {
-	result := run("dev2.txt")
+// func TestDev2(t *testing.T) {
+// 	result := run("dev2.txt")
+
+// 	if result != 6 {
+// 		t.Fatalf("Incorrect result! %d != 6", result)
+// 	}
+// }
+
+// func TestProd(t *testing.T) {
+// 	result := run("test.txt")
+
+// 	if result != 18113 {
+// 		t.Fatalf("Incorrect result! %d != 18113", result)
+// 	}
+// }
+
+func TestDev3(t *testing.T) {
+	result := run2("dev3.txt")
 
 	if result != 6 {
 		t.Fatalf("Incorrect result! %d != 6", result)
 	}
 }
 
-func TestProd(t *testing.T) {
-	result := run("test.txt")
+func TestProd2(t *testing.T) {
+	result := run2("test.txt")
 
-	if result != 18113 {
-		t.Fatalf("Incorrect result! %d != 18113", result)
-	}
+	fmt.Printf("part 2: %d\n", result)
 }
-
-// func TestDev3(t *testing.T) {
-// 	result := run2("dev.txt")
-
-// 	if result != 5905 {
-// 		t.Fatalf("Incorrect result! %d != 5905", result)
-// 	}
-// }
-
-// func TestProd2(t *testing.T) {
-// 	result := run2("test.txt")
-
-// 	fmt.Printf("part 2: %d", result)
-// 	// 248238926: too high
-// }
